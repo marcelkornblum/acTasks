8da2b1eb46293e02f3daf30dfaf38f8d23d24f0a
Marcel Kornblum <me@marcelkornblum.com>
2012-11-11 02:21:43 -0500

    all services cached locally

--- app/js/services.js
+++ app/js/services.js
@@ -11,59 +11,94 @@
 var acServices = angular.module('acServices', ['ngResource']);
 
 acServices.factory('Projects', function($resource) { 
-    var data;
-    var resource = $resource(localStorage.api_url + '?path_info=projects&format=json&auth_api_token=' + localStorage.api_key); 
+  var data;
+  var resource = $resource(localStorage.api_url + '?path_info=projects&format=json&auth_api_token=' + localStorage.api_key); 
 
-    var projects = function() {
-        data = resource.query();
-        return data;
+  var results = function() {
+    if(!data) {
+      data = resource.query();
     }
+    return processProjects();
+  }
 
-    var processProjects = function() {
-        if (data.length> 0 && data[0].processed != true)
-        {
-          for (var i=0; i<data.length; i++)
-          {
-            var pieces = data[i].urls.view.split('/');
-            data[i].slug = pieces[pieces.length - 1];
-            data[i].processed = true;
-          }
-        }
-        return data;
+  var processProjects = function() {
+    if (data.length > 0 && data[0].processed != true)
+    {
+      for (var i=0; i<data.length; i++)
+      {
+        var pieces = data[i].urls.view.split('/');
+        data[i].slug = pieces[pieces.length - 1];
+        data[i].processed = true;
+      }
     }
+    return data;
+  }
 
-    
-    return {
-        query: function() {
-            if(data) {
-                // console.log("returning cached data");
-                return processProjects(data);
-            } else {
-                console.log("getting projects from server");
-                return projects(); 
-            }
-
-        }
-      };
-  });
+  return {
+    query: function() {
+      return results(); 
+    }
+  };
+});
 
 
 acServices.factory('Project', function($resource) { 
-    var resource = $resource(localStorage.api_url + '?path_info=projects/:projectSlug&format=json&auth_api_token=' + localStorage.api_key); 
-    return resource;
-  });
+  var data = Array();
+  var resource = $resource(localStorage.api_url + '?path_info=projects/:projectSlug&format=json&auth_api_token=' + localStorage.api_key); 
+  //return resource;
+
+  var results = function(obj) {
+    if(!data[obj.projectSlug]) {
+      data[obj.projectSlug] = resource.query(obj);
+    }
+    return data[obj.projectSlug];
+  }
+
+  return {
+    get: function(obj) {
+      return results(obj); 
+    }
+  };
+
+});
 
 
 acServices.factory('Tasks', function($resource) { 
-    var resource = $resource(localStorage.api_url + '?path_info=projects/:projectSlug/tasks&format=json&auth_api_token=' + localStorage.api_key); 
-    return resource;
-  });
+  var data;
+  var resource = $resource(localStorage.api_url + '?path_info=projects/:projectSlug/tasks&format=json&auth_api_token=' + localStorage.api_key); 
+
+  var results = function() {
+    if(!data) {
+      data = resource.query();
+    }
+    return data;
+  }
+
+  return {
+    query: function() {
+      return results(); 
+    }
+  };
+});
 
 
 acServices.factory('Task', function($resource) { 
-    var resource = $resource(localStorage.api_url + '?path_info=projects/:projectSlug/tasks/:taskId&format=json&auth_api_token=' + localStorage.api_key); 
-    return resource;
-  });
+  var data = Array();
+  var resource = $resource(localStorage.api_url + '?path_info=projects/:projectSlug/tasks/:taskId&format=json&auth_api_token=' + localStorage.api_key); 
+
+  var results = function(obj) {
+    if(!data[obj.projectSlug]) {
+      data[obj.projectSlug] = resource.query(obj);
+    }
+    return data[obj.projectSlug];
+  }
+
+  return {
+    get: function(obj) {
+      return results(obj); 
+    }
+  };
+});
 
 
 
@@ -101,7 +136,7 @@ Auth.factory('Auth', function() {
     test: function(api_url, api_key, $http) {
       var testString = api_url + '?path_info=info&format=json&auth_api_token=' + api_key;
       $http({method: 'GET', url: testString}).
-       success(function(data, status, $scope) {
+      success(function(data, status, $scope) {
             // $scope.status = status;
             // $scope.data = data;
             localStorage.api_url = api_url
@@ -109,14 +144,14 @@ Auth.factory('Auth', function() {
             localStorage.tested = true
             alert('API URL/Key pair successfully connected');
           }).
-          error(function(data, status) {
+      error(function(data, status) {
             // $scope.data = data || "Request failed";
             // $scope.status = status;
             localStorage.removeItem('api_url')
             localStorage.removeItem('api_key')
             localStorage.tested = false
             alert('API URL/Key pair could not be used to connect');
-        });
+          });
 
     }
   }

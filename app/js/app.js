a78b0fc1cd3f4cbbb54b04853e49ed4593275e99
Marcel Kornblum <marcel@stinkdigital.com>
2012-11-27 18:51:14 -0500

    task ordering and filtering, more task details brought onto list

--- app/js/app.js
+++ app/js/app.js
@@ -14,11 +14,11 @@ angular.module('acTasks', ['acServices', 'acFilters', 'acDirectives', 'Auth']).
     $routeProvider.
         when('/login', {templateUrl: 'partials/login.html',   controller: AuthCtrl}).
         when('/combined', {templateUrl: 'partials/combined.html',   controller: CombinedListCtrl}).
-        when('/projects', {templateUrl: 'partials/project-list.html',   controller: ProjectListCtrl}).
-        when('/projects/:projectSlug', {templateUrl: 'partials/project-detail.html', controller: ProjectDetailCtrl}).
-        when('/projects/:projectSlug/overview', {templateUrl: 'partials/project-overview.html',   controller: ProjectOverviewCtrl}).
-        when('/projects/:projectSlug/tasks', {templateUrl: 'partials/task-list.html',   controller: TaskListCtrl}).
-        when('/projects/:projectSlug/tasks/:taskId', {templateUrl: 'partials/task-detail.html', controller: TaskDetailCtrl}).
+        // when('/projects', {templateUrl: 'partials/project-list.html',   controller: ProjectListCtrl}).
+        // when('/projects/:projectSlug', {templateUrl: 'partials/project-detail.html', controller: ProjectDetailCtrl}).
+        // when('/projects/:projectSlug/overview', {templateUrl: 'partials/project-overview.html',   controller: ProjectOverviewCtrl}).
+        // when('/projects/:projectSlug/tasks', {templateUrl: 'partials/task-list.html',   controller: TaskListCtrl}).
+        // when('/projects/:projectSlug/tasks/:taskId', {templateUrl: 'partials/task-detail.html', controller: TaskDetailCtrl}).
         otherwise({redirectTo: '/login'});
     }]);
   // .run ($rootScope, $location, User) ->

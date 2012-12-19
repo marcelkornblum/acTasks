c54cf32b122ae4b1ce752277bc705977ec9b8a5e
Marcel Kornblum <me@marcelkornblum.com>
2012-12-19 16:04:39 +0000

    rationalised login/auth functionality

--- app/js/app.js
+++ app/js/app.js
@@ -5,10 +5,5 @@ angular.module('acTasks', ['acServices', 'acFilters', 'acDirectives', 'Auth', 'u
     $routeProvider.
         when('/login', {templateUrl: 'partials/login.html',   controller: AuthCtrl}).
         when('/combined', {templateUrl: 'partials/combined.html',   controller: CombinedListCtrl}).
-        // when('/projects', {templateUrl: 'partials/project-list.html',   controller: ProjectListCtrl}).
-        // when('/projects/:projectSlug', {templateUrl: 'partials/project-detail.html', controller: ProjectDetailCtrl}).
-        // when('/projects/:projectSlug/overview', {templateUrl: 'partials/project-overview.html',   controller: ProjectOverviewCtrl}).
-        // when('/projects/:projectSlug/tasks', {templateUrl: 'partials/task-list.html',   controller: TaskListCtrl}).
-        // when('/projects/:projectSlug/tasks/:taskId', {templateUrl: 'partials/task-detail.html', controller: TaskDetailCtrl}).
-        otherwise({redirectTo: '/login'});
+        otherwise({redirectTo: '/combined'});
     }]);
